7e213686ac2f67dc641df3eb76f128b7c08912af
multiplex55 <multiplex55@yahoo.com>
2020-09-10 19:33:13 -0400

    Initial commit

---
+++ ActionRPG/Overlap/Hurtbox.gd
@@ -0,0 +1,38 @@
+extends Area2D
+
+
+const HitEffect = preload("res://Effects/HitEffect.tscn")
+
+var invincible = false setget set_invincible
+
+signal invinciblity_started
+signal invincibility_ended
+
+onready var timer = $Timer
+
+func set_invincible(value):
+	invincible = value
+	if invincible:
+		emit_signal("invinciblity_started")
+	else:
+		emit_signal("invincibility_ended")
+		
+func start_invincibillity(duration):
+	self.invincible = true
+	timer.start(duration)
+		
+func create_hitEffect():	
+	var effect = HitEffect.instance()
+	var main = get_tree().current_scene
+	main.add_child(effect)
+	effect.global_position = global_position
+
+func _on_Timer_timeout():
+	# This will call the setter
+	self.invincible = false
+
+func _on_Hurtbox_invinciblity_started():
+	set_deferred("monitorable",false)	
+
+func _on_Hurtbox_invincibility_ended():
+	monitorable = true
